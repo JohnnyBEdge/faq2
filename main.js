b76a2777e0148800ed5706981fdea349a33b4c41
John Martin <jrmartin182@gmail.com>
2019-06-04 17:39:51 -0600

    created search function

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-//toggles active class
-let categories = document.getElementsByClassName("category");
 
-for (var i = 0; i < categories.length; i++) {
-    categories[i].addEventListener("click", function() {
-      var current = document.getElementsByClassName("active");
-      current[0].className = current[0].className.replace(" active", "");
-      this.className += " active";
-    });
-  } 
+let searchBar = document.getElementById("searchBar");
+searchBar.addEventListener('keyup', filterQuestions);
 
+function filterQuestions(){
+  let filterValue = document.getElementById("searchBar").value.toUpperCase();
+  let categories = document.getElementById("categories");
+  let questions = categories.querySelectorAll('li.collection-item');
 
-
+  for(let i=0; i < questions.length;i++){
+    let p = questions[i].getElementsByTagName('p')[0];
+    if(p.innerHTML.toUpperCase().indexOf(filterValue) > -1){
+      questions[i].style.display = "";
+    } else{
+      questions[i].style.display = "none";
+    }
+  }
+}
