aaab8410341635c6d528560c75e93f82d8c1722e
John Martin <jrmartin182@gmail.com>
2019-06-12 21:02:40 -0600

    remade faq with more organized containers

--- main.js
+++ main.js
@@ -1,38 +1,66 @@
-// let acc = document.getElementsByClassName("category");
-// let i;
+let searchBar = document.getElementById('searchBar');
 
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-
-//     let panel = this.nextElementSibling;
-//     if (panel.style.display === "block") {
-//       panel.style.display = "none";
-//     } else {
-//       panel.style.display = "block";
-//     }
-//   });
-// }
-
-let filter = document.getElementById("searchBar");
-
-filter.addEventListener('keyup', filterQs);
+searchBar.addEventListener('keyup', filterQs);
 
 function filterQs(){
-  let filterValue = document.getElementById("searchBar").value.toUpperCase();
+  let filterValue = searchBar.value.toUpperCase();
 
-  let a = document.querySelectorAll('div.question > a');
-  let category = document.getElementsByClassName("category");
+  let a = document.querySelectorAll("div.child > a");
 
   for(i=0;i<a.length;i++){
-    if(a[i].innerHTML.toUpperCase().indexOf(filterValue) > -1){
+    if(a[i].innerHTML.toUpperCase().indexOf(filterValue)> -1){
       a[i].style.display = 'block';
-      a[i].style.backgroundColor= "rgb(169, 236, 138)";
-      // this.category[j].style.backgroundColor = 'red'
-      // this.category.style.backgroundColor= "rgb(169, 236, 138)";    
     } else {
-        a[i].style.display = "none";
-      }
+      a[i].style.display = 'none';
     }
   }
+}
+
+  let sections = document.getElementsByTagName('section');
+
+  for(j=0;j<sections.length;j++){
+    sections[j].addEventListener('click', openClose);
+    function openClose(){
+      this.classList.toggle('close');
+    }
+  }
+
+  
+  
+  
+
+
+
+
+
+
+// let category =document.getElementsByClassName("category");
+
+// for(x=0;x<category.length;x++){
+//   category[x].addEventListener("click",function() {
+//     this.classList.toggle('open')
+//   let sibs = this.nextElementSibling;
+  
+//   })
+// }
+// //search function
+// let filter = document.getElementById("searchBar");
+
+// filter.addEventListener('keyup', filterQs);
+
+// function filterQs(){
+//   let filterValue = document.getElementById("searchBar").value.toUpperCase();
+
+//   let a = document.querySelectorAll('div.question > a');
+//   let category = document.getElementsByClassName("category");
+
+//   for(i=0;i<a.length;i++){
+//     if(a[i].innerHTML.toUpperCase().indexOf(filterValue) > -1){
+//       a[i].style.display = 'block';
+//       a[i].style.backgroundColor= "rgb(169, 236, 138)";
+//     } else {
+//         a[i].style.display = "none";
+//       }
+//     }
+//   }
  
